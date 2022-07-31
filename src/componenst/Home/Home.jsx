d9b6a164c310fb1c5d7407de124e5cb74eae3b77
Md. Shahriar Kabir <shahriar1904@cseku.ac.bd>
2022-07-31 22:31:32 +0600

    initialized files

---
+++ src/componenst/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from '../shared/NavBar';
+
+function Home(props) {
+    return (
+        <div>
+            <NavBar />
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/componenst/shared/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NavBar(props) {
+    return (
+        <div>
+            fwegrgw
+        </div>
+    );
+}
+
+export default NavBar;
