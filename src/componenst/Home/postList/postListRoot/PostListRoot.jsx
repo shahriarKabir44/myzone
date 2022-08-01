99d18c05c72a8434539b4f3c4346910dbd108169
Md. Shahriar Kabir <shahriar1904@cseku.ac.bd>
2022-08-01 17:01:01 +0600

    made sticky navbar

--- src/componenst/Home/postList/postListRoot/PostListRoot.jsx
+++ src/componenst/Home/postList/postListRoot/PostListRoot.jsx
@@ -5,7 +5,9 @@ function PostListRoot(props) {
     return (
         <div className='postsView'>
             <PostItem />
-
+            <PostItem />
+            <PostItem />
+            <PostItem />
 
         </div>
     );
--- src/componenst/shared/NavBar/NavBar.jsx
+++ src/componenst/shared/NavBar/NavBar.jsx
@@ -8,22 +8,24 @@ import NotificationsSharpIcon from '@mui/icons-material/NotificationsSharp';
 const stockImageURL = "https://imageio.forbes.com/specials-images/imageserve/6109550f1aa8564670194ad4/Close-up-smiling-businesswoman-holding-computer-tablet--looking-to-side/960x0.jpg?format=jpg&width=960"
 function NavBar(props) {
     return (
-        <div className='nabvarRoot'>
-            <div className="siteName">
-                <p className="siteNameTxt">MyZone</p>
-            </div>
-            <div className="searchBar">
-                <input type="text" name="" id="" className="searchBarInput" />
-                <SearchOutlinedIcon className='searchBtn' />
-            </div>
-            <div className="otherOptions">
-                <AppsSharpIcon fontSize='10' className="menuBtn menuButton" />
-                <QuestionAnswerOutlinedIcon fontSize='10' className="menuBtn messageBtn" />
-                <NotificationsSharpIcon fontSize='10' className="menuBtn notifBtn" />
-                <button className="menuBtn profileBtn">
-                    <img className='profileImageNavBar' src={stockImageURL} alt="profile pic" />
-                </button>
+        <div className="navBarContainer">
+            <div className='nabvarRoot'>
+                <div className="siteName">
+                    <p className="siteNameTxt">MyZone</p>
+                </div>
+                <div className="searchBar">
+                    <input type="text" name="" id="" className="searchBarInput" />
+                    <SearchOutlinedIcon className='searchBtn' />
+                </div>
+                <div className="otherOptions">
+                    <AppsSharpIcon fontSize='10' className="menuBtn menuButton" />
+                    <QuestionAnswerOutlinedIcon fontSize='10' className="menuBtn messageBtn" />
+                    <NotificationsSharpIcon fontSize='10' className="menuBtn notifBtn" />
+                    <button className="menuBtn profileBtn">
+                        <img className='profileImageNavBar' src={stockImageURL} alt="profile pic" />
+                    </button>
 
+                </div>
             </div>
         </div>
     );
