30c04c429e494900edb55c3f25a245165c054de5
Md. Shahriar Kabir <shahriar1904@cseku.ac.bd>
2022-08-25 04:16:35 +0600

    fixed styles

---
+++ src/components/shared/SlideInMessageContainer/MessagesContainer/ConversationListRoot.jsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import OpenWithIcon from '@mui/icons-material/OpenWith';
+import './ConversationListRoot.css';
+let conversationList = [
+    {
+        senderId: "1",
+        senderInfo: {
+            profileImageURL: "https://biz30.timedoctor.com/images/2019/08/remote-employee-software.jpg",
+            name: "Rahul Kabir"
+        },
+        message: "Hello!",
+        conversationId: 1,
+        time: (new Date()) * 1
+    },
+    {
+        senderId: "1",
+        senderInfo: {
+            profileImageURL: "https://www.humanrights.vic.gov.au/static/6a24f35b5bd855d2b82601b7e130d239/ecd90/IMG-Hub_Employee_workplace_rights.jpg",
+            name: "Monir Islam"
+        },
+        message: "Good Day!",
+        conversationId: 1,
+        time: (new Date()) * 1
+    },
+    {
+        senderId: "1",
+        senderInfo: {
+            profileImageURL: "https://assets.entrepreneur.com/content/3x2/2000/1655744650-shutterstock-1350369713.jpg?auto=webp&quality=95&crop=2:1&width=400",
+            name: "Imran Kabir"
+        },
+        message: "Hi!",
+        conversationId: 1,
+        time: (new Date()) * 1
+    },
+    {
+        senderId: "1",
+        senderInfo: {
+            profileImageURL: "https://biz30.timedoctor.com/images/2019/08/remote-employee-software.jpg",
+            name: "Rahul Kabir"
+        },
+        message: "Hello!",
+        conversationId: 1,
+        time: (new Date()) * 1
+    },
+    {
+        senderId: "1",
+        senderInfo: {
+            profileImageURL: "https://www.humanrights.vic.gov.au/static/6a24f35b5bd855d2b82601b7e130d239/ecd90/IMG-Hub_Employee_workplace_rights.jpg",
+            name: "Monir Islam"
+        },
+        message: "Good Day!",
+        conversationId: 1,
+        time: (new Date()) * 1
+    },
+    {
+        senderId: "1",
+        senderInfo: {
+            profileImageURL: "https://assets.entrepreneur.com/content/3x2/2000/1655744650-shutterstock-1350369713.jpg?auto=webp&quality=95&crop=2:1&width=400",
+            name: "Imran Kabir"
+        },
+        message: "Hi!",
+        conversationId: 1,
+        time: (new Date()) * 1
+    },
+]
+function ConversationListRoot(props) {
+    return (
+        <div className='conversationListRoot'>
+            <div className="conversationListHeading">
+                <p className="headingTextSlideMessage">Messages</p>
+                <div>
+                    <OpenWithIcon />
+                </div>
+            </div>
+            <div className="conversationListContainer">
+                {conversationList.map((conversation, index) => {
+                    return <ConversationListItem conversation={conversation} key={index} />
+                })}
+            </div>
+        </div>
+    );
+}
+function ConversationListItem(props) {
+    return (
+        <div className='conversationContainer'>
+            <div className="conversationImgContainer">
+                <img src={props.conversation.senderInfo.profileImageURL} alt="" className="userImg" />
+            </div>
+            <div className="infoContainer">
+                <p className="senderName">{props.conversation.senderInfo.name}</p>
+                <p className="messageBody">{props.conversation.message}</p>
+                <p className="messageTime">{props.conversation.time}</p>
+            </div>
+        </div>
+    );
+}
+export default ConversationListRoot;
--- src/components/shared/SlideInMessageContainer/SlideInMessagesRoot/SlideInMessagesRoot.jsx
+++ src/components/shared/SlideInMessageContainer/SlideInMessagesRoot/SlideInMessagesRoot.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import ConversationListRoot from '../MessagesContainer/ConversationListRoot/ConversationListRoot';
+import ConversationListRoot from '../MessagesContainer/ConversationListRoot';
 import './SlideInMessagesRoot.css'
 
 
 function SlideInMessagesRoot(props) {
     const toggleStatus = useSelector(state => state.conversationListToggleManager.value.status)
     return (
-        <div className={`SlideInMessagesRoot ${toggleStatus === 1 ? 'slideContainerRight' : toggleStatus === 0 ? 'slideContainerLeft' : ""}`}>
+        <div style={{
+            background: "#47494a"
+        }} className={`SlideInMessagesRoot ${toggleStatus === 1 ? 'slideContainerRight' : toggleStatus === 0 ? 'slideContainerLeft' : ""}`}>
 
             <ConversationListRoot />
 
